339ad6877cec13ada2bbcd2835daf66f05a05f94
Gustavo Garcia <goostavobg@gmail.com>
2022-06-16 21:50:29 -0300

    (cart/purchase test) created skeleton

---
+++ test/controllers/cart/purchase.test.js
@@ -0,0 +1,18 @@
+/**
+ * Cat/Purchase Test
+ *
+ * @description :: Testing for Cart/Purchase services
+ */
+
+var assert = require('chai').assert;
+
+
+describe('CartController', () => {
+  describe('#Purchase', () => {
+
+    it('Fail test', () => {
+      assert.fail();
+    });
+
+  });
+});
