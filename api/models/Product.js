6adfde9f915443fb57ebc846106827d0aca83742
Gustavo Garcia <goostavobg@gmail.com>
2022-06-16 21:11:58 -0300

    Product Model

---
+++ api/models/Product.js
@@ -0,0 +1,43 @@
+/**
+ * Product.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    sku: {
+      description: 'unique product ID (Stock Keeping Unit)',
+      type: 'string',
+      unique: true
+    },
+    label: {
+      description: 'Pretty Product Name',
+      type: 'string',
+      defaultsTo: ''
+    },
+    quantity: {
+      description: 'Stock quantity available for purchase',
+      type: 'number',
+      defaultsTo: 0
+    }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
