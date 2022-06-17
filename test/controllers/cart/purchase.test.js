c6d30b01ffc16f73d03b359e8b9cd91fda8793c1
Gustavo Garcia <goostavobg@gmail.com>
2022-06-16 22:09:38 -0300

    (cart/purchase test) added fixtures

--- test/controllers/cart/purchase.test.js
+++ test/controllers/cart/purchase.test.js
@@ -1,18 +1,38 @@
 /**
- * Cat/Purchase Test
+ * Cart/Purchase Test
  *
  * @description :: Testing for Cart/Purchase services
  */
 
 var assert = require('chai').assert;
+const supertest = require('supertest');
 
+const products = [
+  {
+    sku: '123',
+    label: 'Ram memmory 8GB',
+    quantity: 10,
+    price: 45.5,
+  },
+  {
+    sku: '224',
+    label: 'I5 CPU',
+    quantity: 0,
+    price: 99.99
+  },
+];
 
 describe('CartController', () => {
-  describe('#Purchase', () => {
+  beforeEach(async () => {
+    Product.destroy({});
+    Product.createEach(products);
+  });
 
-    it('Fail test', () => {
+  describe('#Purchase', () => {
+    it('Purchase a single product in stock', async () => {
+      const result = supertest(sails.hooks.http.app).post('/cart/purchase');
+
       assert.fail();
     });
-
   });
 });
